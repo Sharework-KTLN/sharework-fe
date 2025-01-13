0255f1bdc19904390203008d9449eec52d72d7bb
Hiep Phan <phhiep999@gmail.com>
2025-01-13 13:32:57 +0700

    config linting rules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+const name = 'John'; // Lỗi: thiếu dấu chấm phẩy
+
+
 const Header = () => {
     return (
         <div>
-            <h1>Hello World!</h1>
+            <h1>Hello {name}!</h1>
         </div>
     );
-}
+};
 
 export default Header;
