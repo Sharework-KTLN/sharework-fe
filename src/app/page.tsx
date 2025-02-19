31885e695c01097e85c92064cdc23f2ce143a373
Nhut Hao <c520a1@gmail.com>
2025-02-19 18:10:23 +0700

    save changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 // import NavigationBar from "@/components/NavigationBar";
-import Homes from "@/app/candidate/home/page";
+// import Homes from "@/app/candidate/home/page";
+import Homes from "@/app/candidate/recruitmentInfoDetail/page"
 export default function Home() {
   return (
     <div>
