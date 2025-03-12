8acfe1ffee780ce20afe48a33f692d16cfd701dd
Hiep Phan <phhiep999@gmail.com>
2025-03-12 11:35:35 +0700

    create redux for user

---
+++ src/redux/store.tsx
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './userSlice';
+
+export const store = configureStore({
+    reducer: userReducer
+});
+
+// Kiểu dữ liệu của Redux Store
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
